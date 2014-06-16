87b03a2f36de71d0103d8b83efddc61a5fed2619
BensonZc <78127000@qq.com>
2014-06-16 11:17:43 +0800

    add
    add .bat file  and update cleardb.php for deleting uploads files

--- application/controllers/cleardb.php
+++ application/controllers/cleardb.php
@@ -3,6 +3,11 @@ class ClearDB extends CI_Controller {
 
 	public function deleteTableDaily(){
 		$this->load->database();
+		$this->load->helper('file');
+		/*delete file*/
+		delete_files('./uploads');
+		
+		/*clear db*/
 		$query = $this->db->query("Select CONCAT('DROP TABLE ', table_name, ';' ) deletequery FROM information_schema.tables Where table_name LIKE '%\_table';");
 		
 		foreach ($query->result() as $row){
